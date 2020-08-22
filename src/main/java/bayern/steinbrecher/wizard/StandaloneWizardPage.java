7f1fdffd355401776e826aa9fe49d77cbb52314b
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-08-22 09:15:38 +0200

    Provide standalone wrapper mechanism

--- src/main/java/bayern/steinbrecher/wizard/StandaloneWizardPage.java
+++ src/main/java/bayern/steinbrecher/wizard/StandaloneWizardPage.java
@@ -1,10 +1,15 @@
 package bayern.steinbrecher.wizard;
 
-import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.LoadException;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -14,18 +19,51 @@ import java.util.ResourceBundle;
  * @author Stefan Huber
  * @since 1.23
  */
-public abstract class StandaloneWizardPage<T extends Optional<?>, C extends StandaloneWizardPageController<T>>
+public abstract class StandaloneWizardPage<T extends Optional<?>, C extends WizardPageController<T>>
         extends WizardPage<T, C> {
-    private final Stage stage;
 
-    protected StandaloneWizardPage(@NotNull String fxmlPath, @Nullable ResourceBundle bundle, Stage stage) {
+    /**
+     * @since 1.26
+     */
+    protected StandaloneWizardPage(@NotNull String fxmlPath, @Nullable ResourceBundle bundle) {
         super(fxmlPath, bundle);
-        this.stage = stage;
     }
 
-    @FXML
-    private void initialize() {
-        getController()
-                .setStage(stage);
+    /**
+     * @since 1.26
+     */
+    public Pane generateStandalonePage(@NotNull Stage stage, @NotNull String closeText) throws LoadException {
+        FXMLLoader fxmlLoader;
+        try {
+            fxmlLoader = FXMLLoader.load(
+                    getClass().getResource("StandaloneWizardPage.fxml"),
+                    ResourceBundle.getBundle("bayern.steinbrecher.wizard.StandaloneWizardPage"));
+        } catch (IOException ex) {
+            throw new LoadException("Could not load the standalone wizard page wrapper description", ex);
+        }
+
+        // Setup controller
+        StandaloneWizardPageController<?> standaloneController = fxmlLoader.getController();
+        standaloneController.setCloseText(closeText);
+        standaloneController.setStage(
+                Objects.requireNonNull(stage, "For being used as a standalone window a stage is required"));
+
+        // Embed wizard page content
+        Pane standaloneRoot = fxmlLoader.getRoot();
+        standaloneRoot.getChildren()
+                .stream()
+                .filter(n -> n.getId().equals("contentHolder"))
+                .filter(n -> n instanceof Pane)
+                .map(n -> (Pane) n)
+                .findFirst()
+                .orElseThrow(() -> new Error(
+                        "Could not insert wizard page into standalone wrapper",
+                        new NoSuchElementException(
+                                "Could not find the element to hold the wizard page content "
+                                        + "(Either no element with the required name is found or no children can "
+                                        + "be added)")))
+                .getChildren()
+                .add(generateEmbeddableWizardPage().getRoot());
+        return standaloneRoot;
     }
 }
--- src/main/java/bayern/steinbrecher/wizard/StandaloneWizardPageController.java
+++ src/main/java/bayern/steinbrecher/wizard/StandaloneWizardPageController.java
@@ -2,18 +2,28 @@ package bayern.steinbrecher.wizard;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 /**
  * @author Stefan Huber
  * @since 1.23
  */
-public abstract class StandaloneWizardPageController<T extends Optional<?>> extends WizardPageController<T> {
-    private final ObjectProperty<Stage> stage = new SimpleObjectProperty<>();
+abstract class StandaloneWizardPageController<T extends Optional<?>> extends WizardPageController<T> {
+    @FXML
+    private ResourceBundle resources;
+    private final ObjectProperty<Stage> stage = new SimpleObjectProperty<>(this, "stage", null);
+    private final StringProperty closeText
+            = new SimpleStringProperty(this, "closeText", resources.getString("close"));
 
+    @NotNull
     public ObjectProperty<Stage> stageProperty() {
         return stage;
     }
@@ -28,11 +38,39 @@ public abstract class StandaloneWizardPageController<T extends Optional<?>> exte
                 .get();
     }
 
+    /**
+     * @since 1.26
+     */
+    @NotNull
+    public StringProperty closeTextProperty() {
+        return closeText;
+    }
+
+    /**
+     * @since 1.26
+     */
+    public void setCloseText(@NotNull String closeText) {
+        closeTextProperty()
+                .set(Objects.requireNonNull(closeText));
+    }
+
+    /**
+     * @since 1.26
+     */
+    public String getCloseText() {
+        return closeTextProperty()
+                .get();
+    }
+
     @FXML
     private void close() {
-        if (isValid()) {
-            getStage()
-                    .close();
+        if (getStage() == null) {
+            throw new IllegalStateException("Can not close stage since there is no stage set");
+        } else {
+            if (isValid()) {
+                getStage()
+                        .close();
+            }
         }
     }
 }
